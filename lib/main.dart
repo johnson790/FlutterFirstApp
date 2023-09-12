60d26c245c7086c802c54b72a843b2006be7b2bc
johnson790 <johnsonykyando1739@gmail.com>
2023-09-12 12:50:00 +0300

    styling text

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
- import 'package:flutter/material.dart';
-  
-void main(){
-runApp(MaterialApp(home: Text('Hello World')));
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 1, 231),
+                Color.fromARGB(255, 45, 0, 80)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'New Application',
+              style: TextStyle(color: Colors.white,fontSize: 28),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
